e9a8659d4a71717ae9fa34e5768c815fc1a498e5
belohlavek <dbhvk.96@gmail.com>
2018-01-18 17:44:14 -0300

    future

--- test/fixtures/support/ClientHelpers.ts
+++ test/fixtures/support/ClientHelpers.ts
@@ -1,5 +1,23 @@
 import { API } from '../../../lib/client';
 
+export type IFuture<T> = Promise<T> & { resolve?: (x: T) => void, reject?: (x: Error) => void };
+
+export function future<T = any>(): IFuture<T> {
+  let resolver: (x: T) => void = (x: T) => { throw new Error("Error initilizing mutex"); };
+  let rejecter: (x: Error) => void = (x: Error) => { throw x; };
+
+  const promise: IFuture<T> = new Promise((ok, err) => {
+    resolver = ok;
+    rejecter = err;
+  });
+
+  promise.resolve = resolver;
+  promise.reject = rejecter;
+
+  return promise;
+}
+
+
 export function test(fn: () => Promise<any>) {
   fn()
     .then((x) => API.Test.pass(x))
