759bde379318580e3bf34940d8a67b138256fa54
Agustin Mendez (Menduz) <agustin.mz.92@gmail.com>
2018-01-25 13:29:43 -0300

    (chore) Style

--- src/client/index.ts
+++ src/client/index.ts
@@ -7,9 +7,11 @@ if (
   throw new Error('Error: Running scripting client outside WebWorker')
 }
 
-new Function(
-  `XMLHttpRequest = function () { throw new Error('XMLHttpRequest is disabled. Please use fetch'); };`
-)()
+new Function(`
+  XMLHttpRequest = function () {
+    throw new Error('XMLHttpRequest is disabled. Please use fetch');
+  };
+`)()
 
 // Exports
 
